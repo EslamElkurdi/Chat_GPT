ea4025daeb1a74c8770f6666ff6a1c8ee4da2c77
ESLAM <elkurdieslam@gmail.com>
2023-01-25 13:48:35 +0200

    Add ListView to show messages in chat screen

--- lib/screens/chatting_screen.dart
+++ lib/screens/chatting_screen.dart
@@ -75,7 +75,16 @@ class _ChatPage extends State<ChatPage> {
       ),
       body: Column(
         children: <Widget>[
-
+          Flexible(
+            child: ListView.builder(
+              reverse: true,
+              padding: const EdgeInsets.all(8.0),
+              itemBuilder: (_, index) {
+                return _messages[index];
+              },
+              itemCount: _messages.length,
+            ),
+          ),
 
         ],
       ),
